ccd8fccf587d785932cba3cde1a499d3bedae5ce
Pankaj <ronhackpankajyadav@gmail.com>
2019-03-06 17:29:40 +0530

    Added failing test.

--- myFunction.test.js
+++ myFunction.test.js
@@ -6,3 +6,10 @@ test('checks add, subtract, multiply, division on 2 and 4', () => {
 	expect(functions.mult(2, 4)).toBe(8);
 	expect(functions.div(2, 4)).toBe(1/2);
 });
+
+test('Add and multiply should fail and sub and div should pass.', () => {
+▷⋅⋅⋅⋅⋅⋅⋅expect(functions.add(2, 4)).toBe(5);
+	expect(functions.sub(2, 4)).toBe(-2);
+▷⋅⋅⋅⋅⋅⋅⋅expect(functions.mult(2, 4)).toBe(6);
+▷⋅⋅⋅⋅⋅⋅⋅expect(functions.div(2, 4)).toBe(1/2);
+});
